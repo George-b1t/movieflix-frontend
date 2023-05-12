46ac4b5f7612922958026ae797caf28ee97cbe4d
GugaAAndrade <gugucp2003@gmail.com>
2023-05-11 22:55:47 -0300

    add: InfoProducts

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -15,54 +15,210 @@ function Products() {
       <Header />
       <h1 className={styles.TitleSnack}>Snack Bar</h1>
 
-      <div className={styles.Tudo}>
+      {/* Linha 1 de Produtos */}
+      <div className={styles.rowPopUp}>
 
+        {/* PopUp Esquerda */}
         <div className={styles.comboPopUp}>
-
+          {/* PopUp Cima Esquerda */}
           <div className={styles.insidePopUp}>
 
-            <div className={styles.insideHalfPopUp}><img src="/combo_baldao.png" alt="" /></div>
-            <div className={styles.insideHalfPopUp}></div>
+          {/* Metade da Esquerda do PopUp Cima Esquerda */}
+            <div className={styles.insideHalfLeftPopUp}><img src="/ProductsImages/combo_baldao.png" alt="" /></div>
+
+          {/* Metade da Direita do PopUp Cima Esquerda */}    
+            <div className={styles.insideHalfRightPopUp}>
+
+              <h1 style={{color:"white"}}>Combo Baldão</h1>
+              
+              <ul className={styles.ListaCombo}>
+                <li>1 Baldão de Pipoca</li>
+                <li>2 Refrigerantes Grande</li>
+              </ul>
+
+            </div>
 
           </div>
-          <div className={styles.comboPopUpButton}></div>
+          
+          {/* Botão Laranja PopUp Esquerda */}
+          <div className={styles.comboPopUpButton}>
+
+          <h3 className={styles.TextAddToCart}>Adicionar ao Carrinho</h3>
+
+          </div>
 
         </div>
 
+        {/* PopUp Direita */}
         <div className={styles.comboPopUp}>
 
-          <div className={styles.comboPopUpButton}></div>
+          {/* PopUp Cima Direita */}
+           <div className={styles.insidePopUp}>
+
+           {/* Metade da Esquerda do PopUp Cima Direita */}
+            <div className={styles.insideHalfLeftPopUp}><img src="/ProductsImages/combo_duplo.png" alt="" /></div>
+
+           {/* Metade da Direita do PopUp Cima Direita */}    
+            <div className={styles.insideHalfRightPopUp}>
+
+            <h1 style={{color:"white"}}>Combo Duplo</h1>
+              
+              <ul className={styles.ListaCombo}>
+                <li>1 Pipoca Grande</li>
+                <li>2 Refrigerantes Grande</li>
+              </ul>
+
+            </div>
+            
+          </div>
+
+          {/* Botão Laranja PopUp Esquerda */}
+          <div className={styles.comboPopUpButton}>
+
+          <h3 className={styles.TextAddToCart}>Adicionar ao Carrinho</h3>
+
+          </div>
 
         </div>
 
       </div>
 
-      <div className={styles.Tudo}>
+      {/* Linha 2 de Produtos */}
+      <div className={styles.rowPopUp}>
 
+        {/* PopUp Esquerda */}
         <div className={styles.comboPopUp}>
 
-          <div className={styles.comboPopUpButton}></div>
+          {/* PopUp Meio Esquerda */}
+          <div className={styles.insidePopUp}>
+
+            {/* Metade da Esquerda do PopUp Meio Esquerda*/}
+            <div className={styles.insideHalfLeftPopUp}><img src="/ProductsImages/combo_super.png" alt="" /></div>
+
+            {/* Metade da Direita do PopUp Meio Esquerda*/}  
+            <div className={styles.insideHalfRightPopUp}>
+
+            <h1 style={{color:"white"}}>Combo Super</h1>
+              
+              <ul className={styles.ListaCombo}>
+                <li>1 Pipoca Grande</li>
+                <li>1 Refrigerantes Super</li>
+              </ul>
+
+            </div>
+
+          </div>
+          
+          {/* Botão Laranja PopUp Esquerda */}
+          <div className={styles.comboPopUpButton}>
+
+          <h3 className={styles.TextAddToCart}>Adicionar ao Carrinho</h3>
+
+          </div>
 
         </div>
+
+        {/* PopUp Direita */}
         <div className={styles.comboPopUp}>
 
-          <div className={styles.comboPopUpButton}></div>
+          {/* PopUp Meio Direita */}
+           <div className={styles.insidePopUp}>
+          
+            {/* Metade da Esquerda do PopUp Meio Direita*/}
+            <div className={styles.insideHalfLeftPopUp}><img src="/ProductsImages/combo_grande.png" alt="" /></div>
+
+            {/* Metade da Direita do PopUp Meio Direita*/}  
+            <div className={styles.insideHalfRightPopUp}>
+
+            <h1 style={{color:"white"}}>Combo Grande</h1>
+              
+              <ul className={styles.ListaCombo}>
+                <li>1 Pipoca Grande</li>
+                <li>1 Refrigerantes Grande</li>
+              </ul>
+
+
+            </div>
+
+          </div>
+
+          {/* Botão Laranja PopUp Direita */}
+          <div className={styles.comboPopUpButton}>
+
+          <h3 className={styles.TextAddToCart}>Adicionar ao Carrinho</h3>
+
+          </div>
 
         </div>
 
       </div>
 
-      <div className={styles.Tudo}>
 
+      {/* Linha 3 de Produtos */}
+      <div className={styles.rowPopUp}>
+
+        {/* PopUp Esquerda */}
         <div className={styles.comboPopUp}>
 
-          <div className={styles.comboPopUpButton}></div>
+          {/* PopUp Baixo Esquerda */}
+          <div className={styles.insidePopUp}>
+
+          {/* Metade da Esquerda do PopUp Baixo Esquerda*/}
+            <div className={styles.insideHalfLeftPopUp}><img src="/ProductsImages/combo_medio.png" alt="" /></div>
+            
+          {/* Metade da Direita do PopUp Baixo Esquerda*/}      
+            <div className={styles.insideHalfRightPopUp}>
+
+            <h1 style={{color:"white"}}>Combo Medio</h1>
+              
+              <ul className={styles.ListaCombo}>
+                <li>1 Pipoca Media</li>
+                <li>1 Refrigerantes Medio</li>
+              </ul>
+
+            </div>
+
+          </div>
+          
+          {/* Botão Laranja PopUp Esquerda */}
+          <div className={styles.comboPopUpButton}>
+
+            <h3 className={styles.TextAddToCart}>Adicionar ao Carrinho</h3>
+          </div>
 
         </div>
+
+        {/* PopUp Direita */}
         <div className={styles.comboPopUp}>
 
-          <div className={styles.comboPopUpButton}></div>
+          {/* PopUp Baixo Direita */}
+           <div className={styles.insidePopUp}>
 
+            {/* Metade da Esquerda do PopUp Baixo Direita*/}
+            <div className={styles.insideHalfLeftPopUp}><img src="/ProductsImages/combo_kids.png" alt="" /></div>
+
+            {/* Metade da Direita do PopUp Baixo Direita*/}
+            <div className={styles.insideHalfRightPopUp}>
+
+              <h1 style={{color:"white"}}>Combo Kids</h1>
+              
+              <ul className={styles.ListaCombo}>
+                <li>1 Pipoca Kids</li>
+                <li>1 Suco Kapo</li>
+              </ul>
+
+            </div>
+
+          </div>
+          
+          {/* Botão Laranja PopUp Direita */}
+          <div className={styles.comboPopUpButton}>
+
+          <h3 className={styles.TextAddToCart}>Adicionar ao Carrinho</h3>
+
+          </div>
+
+        
         </div>
 
       </div>
