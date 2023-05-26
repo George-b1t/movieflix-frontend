fa1429c1df6af90887b1caf4d150a018c4ddd0a7
George <george.soares509@gmail.com>
2023-05-26 13:35:17 -0300

    start: checkout container

--- src/Router.tsx
+++ src/Router.tsx
@@ -14,12 +14,12 @@ import { Movie } from "./pages/Movie"
 const Router = () => (
     <HashRouter>
       <Switch>        
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={Movie} />
         <Route path="/products" exact component={Products} />
         <Route path="/enter" exact component={Enter} />
         <Route path="/cart" exact component={Cart}/>
         <Route path="/movieschedule" exact component={MovieSchedule}/>
-        <Route path="/movies" exact component={Movie}/>
+        <Route path="/movies" exact component={Home}/>
       </Switch>
     </HashRouter>
 )
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -2,111 +2,103 @@ import { useHistory } from "react-router-dom";
 import styles from "./styles.module.scss"
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
+import { useState } from "react";
 
 function Cart(){
     const navigate = useHistory();
+
+    const [isPaying, setIsPaying] = useState(false);
 
     function goCart() {
         navigate.push("/cart");
     }
 
     return (
-            <div className={styles.container}>
-                <Header />
-                <div className={styles.cart}>
-                <div className={styles.carrinho}>
+        <div className={styles.container}>
+            <Header />
+            <div className={styles.cart}>
+            <div className={styles.carrinho}>
+                
+                    <section className={styles.itenscarrinho}>
+                    <h2>Meu carrinho</h2>
+
+                    <article>
+                        <p>Ingresso filme: guardiões da Galáxia</p>
+                        <div className={styles.quantidade}>
+                            <p>Qtd:</p>
+                            <select value={2} name="" id="">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                        </div>
+                    </article>
+
+                    <article>
+                        <p>Combo Guardião</p>
+                        <div className={styles.quantidade}>
+                            <p>Qtd:</p>
+                            <select value={1} name="" id="">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                        </div>
+                    </article>
                     
-                     <section className={styles.itenscarrinho}>
-                     <h2>Meu carrinho</h2>
+                    </section>
 
-                        <article>
-                            <p>Ingresso filme: Guardiões da Galáxia</p>
-                            <div className={styles.quantidade}>
-                                <p>Qtd:</p>
-                                <select name="" id="">
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                </select>
-                            </div>
-                        </article>
+                    <section className={styles.itensvalores}>
 
-                        <article>
-                            <p>Ingresso filme: Guardiões da Galáxia</p>
-                            <div className={styles.quantidade}>
-                                <p>Qtd:</p>
-                                <select name="" id="">
-                                <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                </select>
-                            </div>
-                        </article>
+                    <h2>{isPaying ? "Pagamento" : "Valores"}</h2>
 
-                        <article>
-                            <p>Ingresso filme: Guardiões da Galáxia</p>
-                            <div className={styles.quantidade}>
-                                <p>Qtd:</p>
-                                <select name="" id="">
-                                <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                </select>
-                            </div>
-                        </article>
+                    <article>
                         
-                     </section>
+                        {
+                            isPaying ? (
+                                <>
+                                    <button className={styles.opcaoPagamento}>
+                                        Pix
+                                    </button>
+                                    <button className={styles.opcaoPagamento}>
+                                        Cartão de crédito
+                                    </button>
+                                </>
+                            )
+                            : (
+                                <>
+                                    <div className={styles.itensingressos}>
+                                        <h3>Ingresso filme: guardiões da Galáxia</h3>
+                                        <p>R$ 40,00</p>
+                                    </div>
 
-                     <section className={styles.itensvalores}>
+                                    <div className={styles.line}></div>
 
-                        <h2>Valores</h2>
+                                    <div className={styles.itensingressos}>
+                                        <h3>Combo Guardião</h3>
+                                        <p>R$ 50,00</p>
+                                    </div>
+                                </>
+                            )
+                        }
 
-                        <article>
-                            
-                            <div className={styles.itensingressos}>
-                                <h3>Ingresso filme: guardiões da Galáxia</h3>
-                                <p>R$ 60,00</p>
-                            </div>
+                    </article>
 
-                            <div className={styles.line}></div>
+                    {
+                        !isPaying && <button className={styles.payButton} onClick={() => setIsPaying(true)}>Continuar</button>
+                    }
 
-                            <div className={styles.itensingressos}>
-                                <h3>Ingresso filme: guardiões da Galáxia</h3>
-                                <p>R$ 60,00</p>
-                            </div>
-
-                            <div className={styles.line}></div>
-
-                            <div className={styles.itensingressos}>
-                                <h3>Ingresso filme: guardiões da Galáxia</h3>
-                                <p>R$ 60,00</p>
-                            </div>
-
-                            <div className={styles.line}></div>
-
-                            <div className={styles.itensingressos}>
-                                <h3>Total</h3>
-                                <p>R$ 180,00</p>
-                            </div>
-
-                        </article>
-
-                        
-                        <button>Continuar</button>
-
-                     </section>
-                </div>
-
-                </div>
-                <Footer />
+                    </section>
             </div>
-        )
+
+            </div>
+            <Footer />
+        </div>
+    )
 
 
 
