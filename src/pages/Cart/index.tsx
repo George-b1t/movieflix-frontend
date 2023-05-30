81f3efb8c47ef2a731da57ee47151ac6f065619c
George <george.soares509@gmail.com>
2023-05-30 15:07:07 -0300

    add: empty cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -5,89 +5,94 @@ import { useContext, useState } from "react";
 import { AppContext } from "../../context/AppContext";
 
 function Cart(){
-    const { cart, setCart } = useContext(AppContext);
+	const { cart, setCart } = useContext(AppContext);
 
-    const [isPaying, setIsPaying] = useState(false);
+	const [isPaying, setIsPaying] = useState(false);
 
-    function changeQuantity(index: number, quantity: number) {
-        if (quantity == 0) return;
-        const newItems = [...cart];
-        newItems[index].quantity = quantity;
-        setCart(newItems);
-    }
+	function changeQuantity(index: number, quantity: number) {
+			if (quantity == 0) return;
+			const newItems = [...cart];
+			newItems[index].quantity = quantity;
+			setCart(newItems);
+	}
 
-    return (
-        <div className={styles.container}>
-            <Header />
-            <div className={styles.cart}>
-            <div className={styles.carrinho}>
-                
-                    <section className={styles.itenscarrinho}>
-                    <h2>Meu carrinho</h2>
+	return (
+			<div className={styles.container}>
+				<Header />
 
-                    {
-                        cart.map((item, index) => (
-                            <article key={index}>
-                                <p>{item.name}</p>
-                                <div className={styles.quantidade}>
-                                    <p>Qtd:</p>
-                                    <input type="number" value={item.quantity} onChange={(e) => changeQuantity(index, Number(e.target.value))} />
-                                </div>
-                            </article>
-                        ))
-                    }
-                    
-                    </section>
+				<div className={styles.cart}>
+					{
+						cart.length == 0 ? (
+							<h1 className={styles.emptyCart}>Seu carrinho está vazio</h1>
+						) : (
+							<div className={styles.carrinho}>
+							
+							<section className={styles.itenscarrinho}>
+							<h2>Meu carrinho</h2>
 
-                    <section className={styles.itensvalores}>
+							{
+								cart.map((item, index) => (
+									<article key={index}>
+										<p>{item.name}</p>
+										<div className={styles.quantidade}>
+											<p>Qtd:</p>
+											<input type="number" value={item.quantity} onChange={(e) => changeQuantity(index, Number(e.target.value))} />
+										</div>
+									</article>
+								))
+							}
+							
+							</section>
 
-                    <h2>{isPaying ? "Pagamento" : "Valores"}</h2>
+							<section className={styles.itensvalores}>
 
-                    <article>
-                        
-                        {
-                            isPaying ? (
-                                <>
-                                    <button className={styles.opcaoPagamento}>
-                                        Pix
-                                    </button>
-                                    <button className={styles.opcaoPagamento}>
-                                        Cartão de crédito
-                                    </button>
-                                </>
-                            )
-                            : (
-                                <>
-                                    {cart.map((item, index) => (
-                                        <>
-                                            <div className={styles.itensingressos} key={index}>
-                                                <h3>{item.name}</h3>
-                                                <p>R$ {item.price * item.quantity}</p>
-                                            </div>
+							<h2>{isPaying ? "Pagamento" : "Valores"}</h2>
 
-                                            {index != (cart.length - 1) && <div className={styles.line} />}
-                                        </>
-                                    ))}
-                                </>
-                            )
-                        }
+							<article>
+								{
+									isPaying ? (
+											<>
+													<button className={styles.opcaoPagamento}>
+															Pix
+													</button>
+													<button className={styles.opcaoPagamento}>
+															Cartão de crédito
+													</button>
+											</>
+									)
+									: 
+									(
+										<>
+											{cart.map((item, index) => (
+												<>
+													<div className={styles.itensingressos} key={index}>
+														<h3>{item.name}</h3>
+														<p>R$ {item.price * item.quantity}</p>
+													</div>
 
-                    </article>
+													{index != (cart.length - 1) && <div className={styles.line} />}
+												</>
+											))}
+										</>
+									)
+								}
 
-                    {
-                        !isPaying && <button className={styles.payButton} onClick={() => setIsPaying(true)}>Continuar</button>
-                    }
+							</article>
 
-                    </section>
-            </div>
+							{
+								!isPaying && <button className={styles.payButton} onClick={() => setIsPaying(true)}>Continuar</button>
+							}
 
-            </div>
-            <Footer />
-        </div>
-    )
+							</section>
+						</div>
+						)
+					}
 
+				</div>
 
-
+				<Footer />
+		</div>
+	)
 }
 
 export {Cart}
