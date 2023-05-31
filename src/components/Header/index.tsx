f86e3e076ce74a9cadae2cbb926cc575c4475cd4
George <george.soares509@gmail.com>
2023-05-31 17:34:54 -0300

    add: integrate api

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,35 @@
+import { useState, useEffect } from "react"
+
+import { api } from "../../services/api";
 import styles from "./styles.module.scss"
 
 function Header() {
+  const [ filiais, setFiliais ] = useState([]);
+
+  const [ selectedFilial, setSelectedFilial ] = useState("");
+
+  useEffect(() => {
+    api.get("/filial")
+      .then(response => {
+        setFiliais(response.data.content)
+        setSelectedFilial(response.data.content[0].cidade)
+      });
+  }, [])
+
   return (
     <div className={styles.container}>
       <a href="/"><h1>MOVIEFLIX</h1></a>
 
       <nav>
-        <a href="#">Local: Aracaju</a>
+        <p className={styles.selectFilial}>
+          Local: {selectedFilial}
+
+          <div className={styles.listFiliais}>
+            {filiais.map((filial: any) => (
+              <p onClick={() => setSelectedFilial(filial.cidade)}>{filial.cidade}</p>
+            ))}
+          </div>
+        </p>
         <a href="/#/movies">Filmes</a>
         <a href="/#/products">Snacks</a>
         <a href="/#/cart">Carrinho</a>
